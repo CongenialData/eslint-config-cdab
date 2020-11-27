ccf15cb1dbb5e4202d5d9467079fadfff8729cac
Andreas Olsson <andreas.olsson@cdab.eu>
2020-11-27 13:26:09 +0100

    Configure rules

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var path = require('path');
+var ecmaVersion = 3
 
-module.exports = JSON.parse(fs.readFileSync(path.join(__dirname, '.eslintrc')));
+module.exports = {
+  env: {
+    amd: false,
+    browser: true,
+    es6: true,
+    jasmine: false,
+    mocha: false,
+    node: true
+  },
+
+  'extends': ['./configs/typescript'],
+
+  globals: {
+    BigInt: false,
+    Map: false,
+    Reflect: false,
+    Set: false,
+    Symbol: false
+  },
+
+  parserOptions: {
+    ecmaVersion: ecmaVersion,
+    sourceType: 'script'
+  },
+
+  reportUnusedDisableDirectives: true,
+
+  root: true
+}
